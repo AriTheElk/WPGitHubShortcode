4ece50958268bc5892b9f7d7ad38e84170883044
Garet McKinley <garetmckinley@me.com>
2012-04-25 09:57:23 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## WordPress Facebook Timestamp (WPFBTimestamp)
+A FaceBook inspired "Time Ago" time stamp plugin. Examples: "Yesterday at 1:42pm", "About an hour ago", "August 14 at 4:00am", and more! It automagically selects to display more/less data depending on how recent the post was.
+
+## Installation
+To install the plugin, download the zipball from GitHub and unzip. Upload the `WPFBTimestamp` folder to `wp/wp-content/plugins/`
+
+
+~Current Version:0.1~
---
+++ main.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: WP GitHub Shortcode
+Plugin URI: https://github.com/iGARET/WPGitHubShortcode
+Description: A quick, easy, and sexy shortcode for displaying a "get on github" button in a WordPress post.
+Version: 0.1
+Author: iGARET
+Author URI: http://igaret.com
+*/
+
+
+
+
+include_once('updater.php');
+define('WP_GITHUB_FORCE_UPDATE', true);
+
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+
+	$config = array(
+		'slug' => plugin_basename(__FILE__),
+		'proper_folder_name' => 'WPGitHubShortcode',
+		'api_url' => 'https://api.github.com/repos/iGARET/WPGitHubShortcode',
+		'raw_url' => 'https://raw.github.com/iGARET/WPGitHubShortcode/master',
+		'github_url' => 'https://github.com/iGARET/WPGitHubShortcode',
+		'zip_url' => 'https://github.com/iGARET/WPGitHubShortcode/zipball/master',
+		'sslverify' => true,
+		'requires' => '3.0',
+		'tested' => '3.3.2',
+	);
+	new WPGitHubUpdater($config);
+
+}
+
+add_shortcode("github", "display_github_button");
+function display_github_button($atts)
+{
+	extract(shortcode_atts(array('repo' => ''), $atts));
+	
+	$data = explode("/", $repo);
+	
+	return "<div class='github_button blue'><a href='https://github.com/$repo'>get {$data[1]} on github.com</a></div>";
+}
+
+
+
+add_action('wp_head', 'addHeaderCode');
+function addHeaderCode()
+{
+	print_r('<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/WPGitHubShortcode/css/style.css" />' . "\n");
+}
+
+?>
